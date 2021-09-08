4dc5fbd70d802ca94c7461ea719388bf8018b451
Marco Aerlic <20690053@students.ltu.edu.au>
2021-09-08 16:30:26 +1000

    Java PRB App Demo2

---
+++ src/main/java/au/scott/App.java
@@ -0,0 +1,18 @@
+package au.scott;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static boolean isDivisibleByFive(int number){
+        return number % 5 == 0;
+        } 
+       
+}
